c1ba04b1eb1b8f60b7a72a4f25082214657eef38
Md-Sohag-ALi <sksohagkhan99844889@gmail.com>
2024-06-26 23:26:00 +0600

    LL code

--- linked-list/single-linked-list/Insert_after_a_given_node.c
+++ linked-list/single-linked-list/Insert_after_a_given_node.c
@@ -10,15 +10,15 @@ struct node
 
 struct node *Add_To_Empty(struct node *head,int data)
 {
-   struct node *temp=malloc(sizeof(struct node));
-   temp->data=data;
-   temp->link=NULL;
-   head=temp;
+    struct node *temp=malloc(sizeof(struct node));
+    temp->data=data;
+    temp->link=NULL;
+    head=temp;
 
-   return head;
+    return head;
 }
 
- void Add_at_End(struct node *head,int data)
+void Add_at_End(struct node *head,int data)
 {
     struct node *temp,*ptr;
     temp=malloc(sizeof(struct node));
@@ -36,15 +36,17 @@ struct node *Add_To_Empty(struct node *head,int data)
 void print(struct node *head)
 {
     struct node *ptr=head;
-    while(ptr != NULL){
+    while(ptr != NULL)
+    {
         printf("%d ",ptr->data);
         ptr=ptr->link;
     }
 }
 
 
-struct node *insert_after_a_given_node(struct node *head,int item)
+struct node *insert_after_a_given_node(struct node *head,int node,int item)
 {
+    int flag=0;
     struct node *temp = malloc(sizeof(struct node));
     temp->data=item;
     temp->link=NULL;
@@ -53,40 +55,49 @@ struct node *insert_after_a_given_node(struct node *head,int item)
     ptr=head;
     while(ptr != NULL)
     {
-        if(ptr->data == item)
+        if(ptr->data == node)
         {
             temp->link=ptr->link;
             ptr ->link=temp;
+            flag=1;
             break;
         }
-        else{
+        else
+        {
             ptr=ptr->link;
         }
 
     }
+    if(flag==0)
+    {
+        printf("The node %d is not present in the list :",node);
+    }
     return head;
 }
-int main(){
+int main()
+{
 
-  struct node *head= NULL;
+    struct node *head= NULL;
 
-  head= Add_To_Empty(head,222);
-  Add_at_End(head,52);
-  Add_at_End(head,92);
-  Add_at_End(head,520);
-  Add_at_End(head,23);
-  Add_at_End(head,239);
-  printf("\nThe elements of the linked list :\n");
-  print(head);
+    head= Add_To_Empty(head,222);
+    Add_at_End(head,52);
+    Add_at_End(head,92);
+    Add_at_End(head,520);
+    Add_at_End(head,23);
+    Add_at_End(head,239);
+    printf("\nThe elements of the linked list :\n");
+    print(head);
 
-int item;
-printf("\nEnter a node : \n");
-scanf("%d",&item);
-  head = insert_after_a_given_node(head,item);
+    int item,node;
+    printf("\nEnter a node : \n");
+    scanf("%d",&node);
+    printf("Enter a element to insert after this node : \n");
+    scanf("%d",&item);
+    head = insert_after_a_given_node(head,node,item);
 
-  printf("\nNow The elements of the linked list :\n");
-  print(head);
- return 0;
+    printf("\nNow The elements of the linked list :\n\n\n");
+    print(head);
+    return 0;
 }
 
 
