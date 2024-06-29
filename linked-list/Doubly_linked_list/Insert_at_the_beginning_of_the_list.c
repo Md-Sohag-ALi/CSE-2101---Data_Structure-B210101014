8c91e888ada8888915bdfdbb5d645e7944258857
Md-Sohag-ALi <sksohagkhan99844889@gmail.com>
2024-06-29 14:28:19 +0600

    ok

---
+++ linked-list/Doubly_linked_list/Insert_at_the_beginning_of_the_list.c
@@ -0,0 +1,99 @@
+
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    struct node *prev;
+    int data;
+    struct node *next;
+};
+
+struct node *Add_to_Empty_List(struct node *head,int data)
+{
+    struct node *temp = malloc(sizeof(struct node));
+    temp->prev=NULL;
+    temp->data=data;
+    temp->next=NULL;
+    head=temp;
+
+    return head;
+}
+
+void Add_at_End_of_the_List(struct node * head,int data)
+{
+    struct node *temp=malloc(sizeof(struct node)),*ptr;
+    temp->prev = NULL;
+    temp->data = data;
+    temp->next = NULL;
+
+    ptr=head;
+    while(ptr ->next != NULL)
+    {
+        ptr = ptr->next;
+    }
+
+    ptr->next = temp;
+    temp->prev = ptr;
+
+}
+
+struct node *Create_a_doubly_linked_list(struct node *head)
+{
+    int num_of_node,data;
+    printf("Enter Number of node : \n");
+    scanf("%d",&num_of_node);
+
+    int j=1;
+    printf("Enter Element for node %d :\n",j);
+    scanf("%d",&data);
+    head = Add_to_Empty_List(head,data);
+
+    while(--num_of_node)
+    {
+        printf("Enter element for node %d :\n",j+1);
+        scanf("%d",&data);
+        Add_at_End_of_the_List(head,data);
+        j++;
+    }
+    return head;
+}
+void print(struct node *head)
+{
+    struct node *ptr = head;
+    while(ptr != NULL)
+    {
+        printf("%d ",ptr->data);
+        ptr=ptr->next;
+    }
+
+    printf("\n");
+}
+
+struct node *insert_at_the_begnning_of_the_list(struct node *head,int node)
+{
+    struct node *temp = malloc(sizeof(struct node)),*ptr;
+    temp->prev = NULL;
+    temp->data = node;
+    temp->next = NULL;
+    ptr = head;
+    temp->next = ptr;
+    ptr->prev = temp;
+    head = temp;
+    return head;
+}
+int main()
+{
+    struct node *head=NULL;
+    head = Create_a_doubly_linked_list(head);
+    printf("The elements of the linked list : \n\n");
+    print(head);
+    int node;
+    printf("Enter node : \n");
+    scanf("%d",&node);
+
+    head = insert_at_the_begnning_of_the_list(head,node);
+    print(head);
+    return 0;
+}
