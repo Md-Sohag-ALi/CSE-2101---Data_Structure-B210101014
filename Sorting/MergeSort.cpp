a75310d43b6f2c800f2e2649689ee3316e1fc1a7
Md-Sohag-ALi <sksohagkhan99844889@gmail.com>
2024-12-29 19:32:07 +0600

    Update MergeSort.cpp

--- Sorting/MergeSort.cpp
+++ Sorting/MergeSort.cpp
@@ -1,27 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
-void Merge (int arr[] , int left ,int  mid , int right)
+void Merge (int arr[], int left,int  mid, int right)
 {
-    int nl ,nr;
+    int nl,nr;
     nl = mid -left + 1;
     nr = right - mid ;
-    int L[nl] ,R[nr];
-    for(int i =0;i<nl ;i++)
+    int L[nl],R[nr];
+    for(int i =0; i<nl ; i++)
     {
         L[i] = arr[left + i] ;
     }
-     for(int i =0;i<nr ; i++)
+    for(int i =0; i<nr ; i++)
     {
         R[i] = arr[mid +1+ i] ;
     }
-    int i=0 , j = 0 , k = left;
+    int i=0, j = 0, k = left;
     while(i<nl && j< nr)
     {
         if(L[i] <= R[j])
         {
             arr[k++] = L[i++];
         }
-        else{
+        else
+        {
             arr[k++] = R[j++];
         }
     }
@@ -29,31 +30,32 @@ void Merge (int arr[] , int left ,int  mid , int right)
     {
         arr[k++] = L[i++];
     }
-     while(j<nr)
+    while(j<nr)
     {
         arr[k++] = R[j++];
     }
 
 }
-void mergeSort(int arr[] , int left ,int right)
+void mergeSort(int arr[], int left,int right)
 {
     if(left >= right)
         return;
     int mid = left + (right -left)/2;
-    mergeSort(arr , left , mid);
-    mergeSort(arr , mid + 1 , right);
-    Merge(arr , left ,mid,right);
+    mergeSort(arr, left, mid);
+    mergeSort(arr, mid + 1, right);
+    Merge(arr, left,mid,right);
 }
 int main()
 {
-    int ar_size;cin>>ar_size;
+    int ar_size;
+    cin>>ar_size;
     int arr[ar_size];
-    for(int i=0;i<ar_size;i++)
+    for(int i=0; i<ar_size; i++)
     {
         cin >> arr[i];
     }
-    mergeSort(arr , 0 , ar_size -1);
-    for(int i= 0;i<ar_size ;i++)
+    mergeSort(arr, 0, ar_size -1);
+    for(int i= 0; i<ar_size ; i++)
     {
         cout<<arr[i]<<" ";
     }
